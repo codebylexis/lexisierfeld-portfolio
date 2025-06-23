fb7a9fe13650f9ab8185c9edb63859189f668600
Lexi Sierfeld <lexisierfeld@Lexis-MacBook-Pro-2.local>
2025-06-23 15:39:31 -0400

    Fix MDX config for Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+// next.config.js
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    mdxRs: true
+  },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
 };
 
-export default nextConfig;
+module.exports = withMDX(nextConfig);
